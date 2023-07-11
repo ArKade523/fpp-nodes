ca5e85353044ebd062be6e44710b131ed62e97a0
Kade Angell <ponmlk523@gmail.com>
2023-07-11 14:49:13 -0700

    Updated NodeEditor to use .json files - Added the ability to export .json files - Added the ability to import .json files - Added initial component type creation logic (WIP) - Added the ability to create new nodes

--- src/NodeEditor.svelte
+++ src/NodeEditor.svelte
@@ -6,10 +6,68 @@
   import AreaPlugin from 'rete-area-plugin';
   import NumComponent from './components/NumComponent.js';
   import TextInputComponent from './components/TextInputComponent.js';
-  
+
+  let editor;
+  let file;
+  let nodeCount = 1;
+  let selectedNodeType = ''; // will hold the selected node type
+
+  // this is an array of component classes now
+  let componentClasses = [NumComponent, TextInputComponent];
+  let components = componentClasses.map(ComponentClass => new ComponentClass());
+
+  const addNode = async () => {
+    const component = components.find(c => c.name === selectedNodeType);
+    if (!component) return;
+
+    const node = await component.createNode({ num: nodeCount++ });
+    node.position = [80 * nodeCount, 200];
+    editor.addNode(node);
+  };
+
+  const addComponent = ComponentClass => {
+    componentClasses.push(ComponentClass);
+    const newComponent = new ComponentClass();
+    components.push(newComponent);
+    editor.register(newComponent);
+  };
+
+  const onFileChange = event => {
+    file = event.target.files[0];
+  };
+
+  const loadFile = async () => {
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.onload = async (event) => {
+      const jsonData = JSON.parse(event.target.result);
+      await editor.fromJSON(jsonData);
+    };
+    fileReader.readAsText(file);
+  };
+
+  const exportData = () => {
+    const data = editor.toJSON();
+    const dataStr = JSON.stringify(data, null, 2); // indented with 2 spaces
+    console.log(dataStr);
+
+    const blob = new Blob([dataStr], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    // Create a link and click it to start download
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'data.json';
+    link.click();
+
+    // Remove the link after triggering download
+    link.remove();
+  };
+
   onMount(async () => {
     const container = document.querySelector('#rete');
-    const editor = new Rete.NodeEditor('demo@0.1.0', container);
+    editor = new Rete.NodeEditor('demo@0.1.0', container);
     editor.use(ConnectionPlugin);
     editor.use(AlightRenderPlugin);
     editor.use(AreaPlugin);
@@ -21,13 +79,18 @@
       editor.register(c);
       engine.register(c);
     });
-
-    // Assuming jsonData contains your JSON from the file
-    const response = await fetch('../json/test.json');
-    const jsonData = await response.json();
-
-    await editor.fromJSON(jsonData);
   });
 </script>
 
-<div id="rete"></div>
+<div id="rete">
+  <input type="file" on:change={onFileChange} accept=".json" />
+  <select bind:value={selectedNodeType}>
+    <option disabled selected value> -- select a node type -- </option>
+    {#each components as component (component.name)}
+      <option>{component.name}</option>
+    {/each}
+  </select>
+  <button on:click={addNode}>Add Node</button>
+  <button on:click={loadFile}>Load</button>
+  <button on:click={exportData}>Export</button>
+</div>
