163f443a3698345ac8d283a914026ea7f7421bc1
Kade Angell <ponmlk523@gmail.com>
2023-07-11 11:17:49 -0700

    Added icon, changed window sizing

--- index.cjs
+++ index.cjs
@@ -2,7 +2,11 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    height: 800,
+    width: 1200,
+    icon: path.join(__dirname, "public/fprime.png"),
+  });
   mainWindow.loadFile(path.join(__dirname, "public/index.html"));
   mainWindow.webContents.openDevTools();
 });
